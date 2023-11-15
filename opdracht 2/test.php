6c9ce2379054bd8d40bd3e6b60f3ecfb1194e5b7
9018253 <9018253@student.zadkine.nl>
2023-11-15 09:20:41 +0100

    p2

---
+++ opdracht 2/test.php
@@ -0,0 +1,14 @@
+<?php
+
+// Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "mijn naam is ";
+
+// Voorbeeld van een variabele in PHP.
+$naam = "Edin Mujkic";
+
+// Met echo kun je iets op het scherm tonen.
+echo "$naam";
+
+
+?>
